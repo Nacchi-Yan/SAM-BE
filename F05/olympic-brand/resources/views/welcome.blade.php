62b9f18df8073d8195cb4027506fc48465c7d899
Nacchi-Yan <83501590+Nacchi-Yan@users.noreply.github.com>
2024-12-23 00:12:47 +0800

    updated the href in proj landing page

--- F05/olympic-brand/resources/views/welcome.blade.php
+++ F05/olympic-brand/resources/views/welcome.blade.php
@@ -10,30 +10,33 @@
   <body>
     <h1>Products</h1>
 
-    <div class = "row">
-        @foreach($products as $prod)
-        <div class="col">
-            <div class="card" >
-                @php
-                    $stock = $prod->stock;
-                @endphp
+    <div class="container">
+        <div class="row">
+            @foreach($products as $prod)
+            <div class="col">
+                <div class="card">
+                    @php
+                        $stock = $prod->stock;
+                    @endphp
 
-                @if ($stock>0)
-                    <img src="..." class="card-img-top" alt="...">
-                    <div class="card-body">
-                    <h5 class="card-title">{{ $prod->name }}</h5>
-                    <p class="card-text">{{ $prod->description }}</p>
-                    <p class ="card-text">{{ $prod->price }}</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                @else
-                    <div class="card" style="display: none;">
+                    @if ($stock>0)
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $prod->name }}</h5>
+                            <p class="card-text">{{ $prod->description }}</p>
+                            <p class ="card-text">{{ $prod->price }}</p>
+                            <input type="text" name="quantity"><br><br>
+                            <a href="#" class="btn btn-primary">Add Item</a>
+                        </div>
+                    @else
+                        <div class="card" style="display: none;">
 
-                    </div>
-                @endif
+                        </div>
+                    @endif
+                </div>
             </div>
+            @endforeach
         </div>
-        @endforeach
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
   </body>

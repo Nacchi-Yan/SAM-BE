60bafe9e4a189e9c061b9add47b8cfdecc80024e
Nacchi-Yan <83501590+Nacchi-Yan@users.noreply.github.com>
2024-12-23 17:22:39 +0800

    updated navbar

--- F05/olympic-brand/resources/views/layouts/navbar.blade.php
+++ F05/olympic-brand/resources/views/layouts/navbar.blade.php
@@ -1,1 +1,2 @@
+<a href={{ route('Index') }}><button> HOME</button></a>
 <a href={{ route('orders') }}><button> CART</button></a>
--- F05/olympic-brand/resources/views/receipt.blade.php
+++ F05/olympic-brand/resources/views/receipt.blade.php
@@ -8,6 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body>
+    @include('layouts.navbar')
     <h1>Receipt</h1>
     <!-- Check if user is logged in -->
     @if(session('id'))
@@ -20,7 +21,6 @@
             @endforeach
         </div>
         <h1 class="text-center"> Total </h1>
-        {{-- <button> Submit Order </button> --}}
     @else
     <!-- Redirect back to login if no user session -->
     <script>window.location = "login";</script>
